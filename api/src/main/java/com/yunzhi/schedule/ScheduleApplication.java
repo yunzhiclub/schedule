3bd4c5a9c09c40091d05ca284d0e7e0421ced9b5
chshihang <858190647@qq.com>
2023-01-14 21:53:53 +0800

    v和选择

--- api/src/main/java/com/yunzhi/schedule/ScheduleApplication.java
+++ api/src/main/java/com/yunzhi/schedule/ScheduleApplication.java
@@ -3,13 +3,14 @@ package com.yunzhi.schedule;
 import com.yunzhi.schedule.repository.SoftDeleteRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @ServletComponentScan
 @EnableJpaRepositories(value = "com.yunzhi.schedule",
 		repositoryFactoryBeanClass = SoftDeleteRepositoryFactoryBean.class)
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
 public class ScheduleApplication {
 
 	public static void main(String[] args) {
